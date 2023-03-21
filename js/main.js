54baee8445734135cb843919f23cbbea24a16e0b
Ivana <kostevski.ivana@gmail.com>
2023-03-21 10:54:25 +0100

    Disable transitions on page load and fix menu width for all breakpoints

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+//Disable transitions on page load
+function transitionAfterPageLoad() {
+  document.body.classList.remove("preload");
+}
+
+(function () {
+  transitionAfterPageLoad();
+})();
+
+//Disable transitions on resize
+(function () {
+  const classes = document.body.classList;
+  let timer = 0;
+  window.addEventListener("resize", function () {
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+    } else classes.add("stop-transitions");
+
+    timer = setTimeout(() => {
+      classes.remove("stop-transitions");
+      timer = null;
+    }, 100);
+  });
+})();
+
+//Responsive menu functionality
 const hamburgerBtn = document.getElementById("js-hamburger");
 const topLine = document.getElementById("js-top-line");
 const centerLine = document.getElementById("js-center-line");
