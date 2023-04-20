8ea3fb3777f6298f544df99cd31da0465af19142
Ivana <kostevski.ivana@gmail.com>
2023-04-20 13:18:18 +0200

    Fix additional-recipe-card functionality

--- script.js
+++ script.js
@@ -1,41 +1,81 @@
+//Additional-recipes-card
+
+const additionalRecipesCard = document.querySelector(".additional-recipe-card");
 const fullRecipeButton = document.getElementById("open-recipe-js");
-const recipeHeader = document.querySelector(".recipe-details-header");
-const recipeDetails = document.querySelector(".recipe-details");
-const imgCard = document.querySelector(".recipe-img img");
+const recipeHeader = additionalRecipesCard.querySelector(
+  ".recipe-details-header"
+);
+const recipeDetails = additionalRecipesCard.querySelector(".recipe-details");
+const imgCard = additionalRecipesCard.querySelector(".recipe-img img");
+const prepInfoCard = document.getElementById("prep-info");
 
-var checkButton = false;
+var isRecipeDisplayed = false;
 
-function openFullRecipe() {
-  fullRecipeButton.innerHTML = "CLOSE";
+function openHeader() {
   recipeHeader.classList.add("slide-in-card");
   recipeHeader.classList.remove("slide-out-card");
+}
+
+function closeHeader() {
+  recipeHeader.classList.add("slide-out-card");
+  recipeHeader.classList.remove("slide-in-card");
+}
+
+function openDetails() {
   recipeDetails.classList.add("slide-in-card");
   recipeDetails.classList.remove("slide-out-card");
+}
+
+function closeDetails() {
+  recipeDetails.classList.add("slide-out-card");
+  recipeDetails.classList.remove("slide-in-card");
+}
+
+function showPrepInfo() {
+  prepInfoCard.classList.remove("hide-prep-info");
+}
+
+function hidePrepInfo() {
+  prepInfoCard.classList.add("hide-prep-info");
+}
+
+function handleWindowChange() {
+  if (isRecipeDisplayed === true) {
+    hidePrepInfo();
+  } else {
+    showPrepInfo();
+  }
+}
+
+function openFullRecipe() {
+  isRecipeDisplayed = true;
+  fullRecipeButton.innerHTML = "CLOSE";
+  openHeader();
+  openDetails();
   imgCard.classList.add("move-img");
-  checkButton = true;
+  handleWindowChange();
 }
 
 function closeFullRecipe() {
+  isRecipeDisplayed = false;
   fullRecipeButton.innerHTML = "FULL RECIPE";
-  recipeHeader.classList.add("slide-out-card");
-  recipeHeader.classList.remove("slide-in-card");
-  recipeDetails.classList.add("slide-out-card");
-  recipeDetails.classList.remove("slide-in-card");
+  closeHeader();
+  closeDetails();
   imgCard.classList.remove("move-img");
-  checkButton = false;
+  handleWindowChange();
 }
 
-function openCloseButton() {
-  if (checkButton === false) {
+function toggleRecipeDisplay() {
+  if (isRecipeDisplayed === false) {
     openFullRecipe();
   } else {
     closeFullRecipe();
   }
 }
 
-fullRecipeButton.addEventListener("click", openCloseButton);
+fullRecipeButton.addEventListener("click", toggleRecipeDisplay);
 
-//Tab functionality
+//Additional-recipes-card - Tab functionality
 
 const tabButtons = document.querySelectorAll(".tab-button");
 const tabContents = document.querySelectorAll(".tab-content .tab-pane");
@@ -43,38 +83,15 @@ const tabContents = document.querySelectorAll(".tab-content .tab-pane");
 tabButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const tab = button.dataset.tab;
-
     tabButtons.forEach((button) =>
       button.classList.remove("tab-active", "tab-button-active")
     );
     tabContents.forEach((content) =>
       content.classList.remove("tab-active", "tab-button-active")
     );
-
     button.classList.add("tab-active", "tab-button-active");
     document
       .querySelector(`.tab-content .tab-pane[data-tab="${tab}"]`)
       .classList.add("tab-active");
   });
-});
-
-//prep-info-card responsive
-
-const prepInfoCard = document.getElementById("prep-info");
-const mediaQuery = window.matchMedia("(max-width: 767px)");
-
-function togglePrepInfoCard() {
-  prepInfoCard.classList.toggle("hide-prep-info");
-}
-
-fullRecipeButton.addEventListener("click", function () {
-  if (mediaQuery.matches) {
-    togglePrepInfoCard();
-  }
-});
-
-mediaQuery.addListener(function (event) {
-  if (!event.matches) {
-    prepInfoCard.classList.remove("hide-prep-info");
-  }
 });
