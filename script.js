6e4a9503edc3be19166001f7fdc116b3cd4dc43e
Ivana <kostevski.ivana@gmail.com>
2023-04-25 11:07:05 +0200

    Refacture tabs html and js

--- script.js
+++ script.js
@@ -77,21 +77,63 @@ fullRecipeButton.addEventListener("click", toggleRecipeDisplay);
 
 //Additional-recipes-card - Tab functionality
 
-const tabButtons = document.querySelectorAll(".tab-button");
-const tabContents = document.querySelectorAll(".tab-content .tab-pane");
+// const tabButtons = recipeDetails.querySelectorAll(".tab-button");
+// const tabContents = recipeDetails.querySelectorAll(".tab-pane");
+
+// tabButtons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     const tab = button.dataset.tab;
+//     tabButtons.forEach((button) =>
+//       button.classList.remove("tab-active", "tab-button-active")
+//     );
+//     tabContents.forEach((content) =>
+//       content.classList.remove("tab-active", "tab-button-active")
+//     );
+//     button.classList.add("tab-active", "tab-button-active");
+//     recipeDetails
+//       .querySelector(`.tab-content .tab-pane[data-tab="${tab}"]`)
+//       .classList.add("tab-active");
+//   });
+// });
+
+// 2nd OPTION
+
+// const tabButtons = recipeDetails.querySelectorAll(".tab-button");
+// const tabContents = recipeDetails.querySelectorAll(".tab-pane");
+
+// recipeDetails.addEventListener("click", (event) => {
+//   if (!event.target.matches(".tab-button")) {
+//     return;
+//   }
+//   const tab = event.target.dataset.tab;
+
+//   tabButtons.forEach((button) => {
+//     button.classList.remove("tab-active", "tab-button-active");
+//   });
+//   tabContents.forEach((content) => {
+//     content.classList.remove("tab-active", "tab-button-active");
+//   });
+
+//   event.target.classList.add("tab-active", "tab-button-active");
+//   recipeDetails
+//     .querySelector(`.tab-content .tab-pane[data-tab="${tab}"]`)
+//     .classList.add("tab-active");
+// });
+
+//3rd OPTION
+
+const tabButtons = recipeDetails.querySelectorAll(".tab-button");
+const tabContents = recipeDetails.querySelectorAll(".tab-pane");
 
 tabButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const tab = button.dataset.tab;
-    tabButtons.forEach((button) =>
-      button.classList.remove("tab-active", "tab-button-active")
-    );
+    tabButtons.forEach((button) => {
+      button.classList.toggle("tab-active", button === event.target);
+      button.classList.toggle("tab-button-active", button === event.target);
+    });
     tabContents.forEach((content) =>
-      content.classList.remove("tab-active", "tab-button-active")
+      content.classList.toggle("tab-active", content.dataset.tab === tab)
     );
-    button.classList.add("tab-active", "tab-button-active");
-    document
-      .querySelector(`.tab-content .tab-pane[data-tab="${tab}"]`)
-      .classList.add("tab-active");
   });
 });
