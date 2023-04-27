4942f1cd179f0ed1a5717feebb738f8411ef7e79
Ivana <kostevski.ivana@gmail.com>
2023-04-27 09:51:43 +0200

    Remove commented out code

--- script.js
+++ script.js
@@ -102,51 +102,6 @@ fullRecipeButton.addEventListener("click", toggleRecipeDisplay);
 
 //Additional-recipes-card - Tab functionality
 
-// const tabButtons = recipeDetails.querySelectorAll(".tab-button");
-// const tabContents = recipeDetails.querySelectorAll(".tab-pane");
-
-// tabButtons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     const tab = button.dataset.tab;
-//     tabButtons.forEach((button) =>
-//       button.classList.remove("tab-active", "tab-button-active")
-//     );
-//     tabContents.forEach((content) =>
-//       content.classList.remove("tab-active", "tab-button-active")
-//     );
-//     button.classList.add("tab-active", "tab-button-active");
-//     recipeDetails
-//       .querySelector(`.tab-content .tab-pane[data-tab="${tab}"]`)
-//       .classList.add("tab-active");
-//   });
-// });
-
-// 2nd OPTION
-
-// const tabButtons = recipeDetails.querySelectorAll(".tab-button");
-// const tabContents = recipeDetails.querySelectorAll(".tab-pane");
-
-// recipeDetails.addEventListener("click", (event) => {
-//   if (!event.target.matches(".tab-button")) {
-//     return;
-//   }
-//   const tab = event.target.dataset.tab;
-
-//   tabButtons.forEach((button) => {
-//     button.classList.remove("tab-active", "tab-button-active");
-//   });
-//   tabContents.forEach((content) => {
-//     content.classList.remove("tab-active", "tab-button-active");
-//   });
-
-//   event.target.classList.add("tab-active", "tab-button-active");
-//   recipeDetails
-//     .querySelector(`.tab-content .tab-pane[data-tab="${tab}"]`)
-//     .classList.add("tab-active");
-// });
-
-//3rd OPTION
-
 const tabButtons = recipeDetails.querySelectorAll(".tab-button");
 const tabContents = recipeDetails.querySelectorAll(".tab-pane");
 
